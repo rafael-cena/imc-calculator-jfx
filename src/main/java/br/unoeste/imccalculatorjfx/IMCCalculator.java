0f3accaf50f897d19d7a553956acc15f80e92f99
rafael-cena <rafaelcena4@gmail.com>
2024-08-14 19:35:01 -0300

    Desativado redimensionamento e botoes de maximizar e minimizar removidos!

--- src/main/java/br/unoeste/imccalculatorjfx/IMCCalculator.java
+++ src/main/java/br/unoeste/imccalculatorjfx/IMCCalculator.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -13,6 +14,8 @@ public class IMCCalculator extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(IMCCalculator.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 397, 439);
         stage.setTitle("IMC Calculator FX v0.1");
+        stage.initStyle(StageStyle.UTILITY);
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
